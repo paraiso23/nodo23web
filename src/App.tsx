832eb44b15c12b26952dd214044cc95b434466cb
paraiso23 <45073084+paraiso23@users.noreply.github.com>
2026-04-11 18:07:47 +0100

    revert: restore original App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@
  */
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SkillzHome from './components/SkillzHome';
 import Home from './components/Home';
 import NodoPersonal from './components/NodoPersonal';
 
@@ -12,8 +11,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SkillzHome />} />
-        <Route path="/openclaw" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/nodo-personal" element={<NodoPersonal />} />
       </Routes>
     </Router>
